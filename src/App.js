e3c8f51b4275a4aa8862d10e8e4e745ed4fc03df
obabiy <alex.babiy.sq@gmail.com>
2020-08-06 13:49:24 +0300

    Adding TODOS with useState hook and targets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [todos, setTodos] = useState(['first task', 'second task', 'third task'])
+  const [input, setInput] = useState('');
+  console.log(input)
+
+  const addTodo = (event) => {
+    setTodos([...todos, input])
+  }
+
   return (
     <div className="App">
       <p>TODO application was initialized!</p>
+      <input value = {input} onChange = { event => setInput(event.target.value) }/>   {/* при любом изменении поля идет перезапись input */}
+      <button onClick = {addTodo}>Add Todo</button>
+
+      <ul>
+        {
+          todos.map( todo => (
+            <li>{todo}</li>
+          ))
+        }
+
+      </ul>
     </div>
   );
 }
