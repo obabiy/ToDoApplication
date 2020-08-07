bdca5e7bedecf274abe0a137bd9625e39e66415a
obabiy <alex.babiy.sq@gmail.com>
2020-08-07 08:41:15 +0300

    Added material ui and button fucntionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import { Button } from '@material-ui/core'   // подключаем material ui
+
+
 function App() {
-  
+
   const [todos, setTodos] = useState(['first task', 'second task', 'third task'])
   const [input, setInput] = useState('');
   console.log(input)
 
   const addTodo = (event) => {
-    setTodos([...todos, input])
+    event.preventDefault();       // отключаем перезагрузку страницы после отправки формы 
+    setTodos([...todos, input])  // добавляем(не перезаписываем!) в существующий масив текущее состояние input
+    setInput('')                  // очищаем input после добавление задачи
   }
 
   return (
     <div className="App">
       <p>TODO application was initialized!</p>
-      <input value = {input} onChange = { event => setInput(event.target.value) }/>   {/* при любом изменении поля идет перезапись input */}
-      <button onClick = {addTodo}>Add Todo</button>
+
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />   {/* при любом изменении поля идет перезапись input */}
+        <Button disabled = {!input} type='submit' onClick={addTodo} variant="contained" color="primary"> {/* по клику добавляем input в массив todos и отправляем форму */}
+        Add Todo              {/* отключаем кнопку если input пустой */}
+        </Button>         {/* material ui button */}
+      </form>
 
       <ul>
         {
-          todos.map( todo => (
+          todos.map(todo => (
             <li>{todo}</li>
           ))
         }
