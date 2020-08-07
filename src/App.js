356a82c0550c972c77c7a676521bca4ee5bc5ced
obabiy <alex.babiy.sq@gmail.com>
2020-08-07 10:30:30 +0300

    Remade todos with materialUI and init firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'   // подключаем material ui
+import Todo from './Todo';
+
+
 function App() {
-  
+
   const [todos, setTodos] = useState(['first task', 'second task', 'third task'])
   const [input, setInput] = useState('');
   console.log(input)
 
   const addTodo = (event) => {
-    setTodos([...todos, input])
+    event.preventDefault();       // отключаем перезагрузку страницы после отправки формы 
+    setTodos([...todos, input])  // добавляем(не перезаписываем!) в существующий масив текущее состояние input
+    setInput('')                  // очищаем input после добавление задачи
   }
 
   return (
     <div className="App">
-      <p>TODO application was initialized!</p>
-      <input value = {input} onChange = { event => setInput(event.target.value) }/>   {/* при любом изменении поля идет перезапись input */}
-      <button onClick = {addTodo}>Add Todo</button>
-
+      <h1>TODO Application</h1>
+      
+      <form>
+        <FormControl>
+          <InputLabel>White a TODO</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />  {/* при любом изменении поля идет перезапись input */}
+        </FormControl>
+        
+        <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary"> {/* по клику добавляем input в массив todos и отправляем форму */}
+          Add Todo              {/* отключаем кнопку если input пустой */}
+        </Button>
+      </form>
+      
       <ul>
         {
-          todos.map( todo => (
-            <li>{todo}</li>
+          todos.map(todo => (
+            <Todo text = {todo}/>
           ))
         }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyD_Y1iHbQRFnIoDQ20r1KjQoQHS3xEMFzs",
+  authDomain: "todo-react-app-3215c.firebaseapp.com",
+  databaseURL: "https://todo-react-app-3215c.firebaseio.com",
+  projectId: "todo-react-app-3215c",
+  storageBucket: "todo-react-app-3215c.appspot.com",
+  messagingSenderId: "503513804265",
+  appId: "1:503513804265:web:45a17fa5e8628da0a16f78",
+  measurementId: "G-HWQ53BRN98",
+})
+
+const db = firebaseApp.firestore();
+
+export default db
