4c2861c6dbcf8beac574dcd325f0931013d5ac9a
obabiy <alex.babiy.sq@gmail.com>
2020-08-08 12:34:52 +0300

    Remade design of the app

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,65 +8,84 @@ import {
 	ListItemText,
 	ListItemAvatar,
 	Modal,
-    Button,
-    Input
+	Button,
+	Input,
 } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from "@material-ui/core/styles";
 
 import DeleteForeverIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 
-
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }));
+	paper: {
+        position: "absolute",
+        margin: 'auto',
+        top: 0, bottom: 0, left: 0, right: 0,
+        width: 300,
+        height: 400,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+		outline: "none",
+	},
+}));
 
 function Todo(props) {
-    const classes = useStyles()
-    const [open, setOpen] = useState();
-    const [input, setInput] = useState();
+	const classes = useStyles();
+	const [open, setOpen] = useState();
+	const [input, setInput] = useState();
 
 	const handleOpen = () => {
 		setOpen(true);
-    };
-    
-    const updateTodo = () => {
-        db.collection('todos').doc(props.todo.id).set({
-            task: input
-        }, { merge: true })
-        setOpen(false)
-    }
+	};
+
+	const updateTodo = () => {
+		db.collection("todos").doc(props.todo.id).set(
+			{
+				task: input,
+			},
+			{ merge: true }
+		);
+		setOpen(false);
+	};
 
 	return (
-		<>
-			<Modal open={open} onClose={(e) => setOpen(false)} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
-				<div className = {classes.paper}>
-					<h1>Open</h1>
-                    <Input placeholder={props.todo.task} value={input} onChange = { event => setInput(event.target.value)}/>
-					<Button onClick={ updateTodo } variant="contained">Save</Button>
+		<div id="list_item">
+			<Modal
+				open={open}
+				onClose={(e) => setOpen(false)}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				<div className={classes.paper} id="modal">
+					<h1>Edit task..</h1>
+					<Input
+						placeholder={props.todo.task}
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					/>
+					<Button disabled={!input} onClick={updateTodo} variant="contained">
+						Save
+					</Button>
 				</div>
 			</Modal>
 
 			<List className="todo_list">
 				<ListItem>
-					<ListItemAvatar></ListItemAvatar>
 					<ListItemText primary={props.todo.task} secondary="Dummy deadline" />
 					<EditIcon onClick={handleOpen} />
 					<DeleteForeverIcon
 						onClick={(event) =>
 							db.collection("todos").doc(props.todo.id).delete()
 						}
+						id="deleteButton"
 					/>
 				</ListItem>
 			</List>
-		</>
+		</div>
 	);
 }
 
