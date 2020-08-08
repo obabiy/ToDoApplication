f494a47b7bac197b9bc3d6a21a2100910229006c
obabiy <alex.babiy.sq@gmail.com>
2020-08-08 11:08:59 +0300

    Editing existing docs on db

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core'   // подключаем material ui
-import Todo from './Todo';
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core"; // подключаем material ui
+import Todo from "./Todo";
 
-import db from './firebase'
-import firebase from 'firebase'
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
+	const [todos, setTodos] = useState([]);
+	const [input, setInput] = useState("");
 
-  const [todos, setTodos] = useState([])
-  const [input, setInput] = useState('');
+	// приложение слушает базу данных и при обновлении подгружает новые данн
+	useEffect(() => {
+		//слушаем коллекцию todos принимаем все изменения
+		db.collection("todos")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				//сортируем по датам задачи
+				setTodos(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						task: doc.data().task
+					}))
+				); //разделяем документы => берем поле todo каждого отдельного документа и записываем в локальный массив
+			});
+	}, []);
 
-  // приложение слушает базу данных и при обновлении подгружает новые данн
-  useEffect(() => {       //слушаем коллекцию todos принимаем все изменения
-    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot( snapshot => {  //сортируем по датам задачи  
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, task: doc.data().task})))   //разделяем документы => берем поле todo каждого отдельного документа и записываем в локальный массив
-      console.log(snapshot.docs.map(doc => doc.data().task))
-    })
-  }, []);
+	const addTodo = (event) => {
+		event.preventDefault(); // отключаем перезагрузку страницы после отправки формы
 
-  const addTodo = (event) => {
-    event.preventDefault();       // отключаем перезагрузку страницы после отправки формы 
-    
-    db.collection("todos").add({   // добавляем задание на сервер
-      task: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()  //добавляем к документу время публ. на сервере для сортировки
-    })
-    
-    setTodos([...todos, input])  // добавляем(не перезаписываем!) в существующий масив текущее состояние input
-    setInput('')                  // очищаем input после добавление задачи
-  }
+		db.collection("todos").add({
+			// добавляем задание на сервер
+			task: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(), //добавляем к документу время публ. на сервере для сортировки
+		});
 
-  return (
-    <div className="App">
-      <h1>TODO Application</h1>
-      
-      <form>
-        <FormControl>
-          <InputLabel>White a TODO</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />  {/* при любом изменении поля идет перезапись input */}
-        </FormControl>
-        
-        <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary"> {/* по клику добавляем input в массив todos и отправляем форму */}
-          Add Todo              {/* отключаем кнопку если input пустой */}
-        </Button>
-      </form>
-      
-      <ul>
-        {
-          todos.map(todo => (
-            <Todo todo = {todo}/>
-          ))
-        }
+		setTodos([...todos, input]); // добавляем(не перезаписываем!) в существующий масив текущее состояние input
+		setInput(""); // очищаем input после добавление задачи
+	};
 
-      </ul>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>TODO Application</h1>
+
+			<form>
+				<FormControl>
+					<InputLabel>White a TODO</InputLabel>
+					<Input
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					/>{" "}
+					{/* при любом изменении поля идет перезапись input */}
+				</FormControl>
+
+				<Button
+					disabled={!input}
+					type="submit"
+					onClick={addTodo}
+					variant="contained"
+					color="primary"
+				>
+					{" "}
+					{/* по клику добавляем input в массив todos и отправляем форму */}
+					Add Todo {/* отключаем кнопку если input пустой */}
+				</Button>
+			</form>
+
+			{/* <Button onClick = { event => (db.collection('todos').docs.map( doc => db.collection('todos').doc(doc.id).delete() ))} variant = "contained" color = "secondary">
+        DELETE ALL
+      </Button> */}
+
+			<ul>
+				{todos.map((todo) => (
+					<Todo todo={todo} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
-import './Todo.css'
-import db from './firebase'
+import React, { useState } from "react";
+import "./Todo.css";
+import db from "./firebase";
 
-import { List, ListItem, ListItemText, ListItemAvatar, Button} from "@material-ui/core"
+import {
+	List,
+	ListItem,
+	ListItemText,
+	ListItemAvatar,
+	Modal,
+    Button,
+    Input
+} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles'
+
+import DeleteForeverIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 function Todo(props) {
-    return (
-        <List className = "todo_list">
-            <ListItem> 
-                
-                <ListItemAvatar>
-                </ListItemAvatar>
+    const classes = useStyles()
+    const [open, setOpen] = useState();
+    const [input, setInput] = useState();
 
-                <ListItemText primary = {props.todo.task} secondary = 'Dummy deadline' />
-            
-                <Button onClick={event => db.collection('todos').doc(props.todo.id).delete()} variant = 'contained' color = 'secondary'>Delete me</Button>
-            </ListItem>
-        </List>
-    )
+	const handleOpen = () => {
+		setOpen(true);
+    };
+    
+    const updateTodo = () => {
+        db.collection('todos').doc(props.todo.id).set({
+            task: input
+        }, { merge: true })
+        setOpen(false)
+    }
+
+	return (
+		<>
+			<Modal open={open} onClose={(e) => setOpen(false)} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
+				<div className = {classes.paper}>
+					<h1>Open</h1>
+                    <Input placeholder={props.todo.task} value={input} onChange = { event => setInput(event.target.value)}/>
+					<Button onClick={ updateTodo } variant="contained">Save</Button>
+				</div>
+			</Modal>
+
+			<List className="todo_list">
+				<ListItem>
+					<ListItemAvatar></ListItemAvatar>
+					<ListItemText primary={props.todo.task} secondary="Dummy deadline" />
+					<EditIcon onClick={handleOpen} />
+					<DeleteForeverIcon
+						onClick={(event) =>
+							db.collection("todos").doc(props.todo.id).delete()
+						}
+					/>
+				</ListItem>
+			</List>
+		</>
+	);
 }
 
-export default Todo
+export default Todo;
